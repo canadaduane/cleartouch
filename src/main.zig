a312e4240ee84df5bf9dd11ecf11299633a15907
Duane Johnson <duane.johnson@gmail.com>
2021-11-26 23:59:54 -0700

    Show multitouch circles on gl window

--- src/main.zig
+++ src/main.zig
@@ -10,72 +10,14 @@ const ray = @cImport({
 
 const ORANGE =
     ray.Color{ .r = 255, .g = 161, .b = 0, .a = 255 };
-const MAX_TOUCH_POINTS = 10;
-const SCREEN_WIDTH = 800;
-const SCREEN_HEIGHT = 450;
-
-// pub fn main() anyerror!void {
-// const fd = try udev.open_touchpad();
-// defer os.close(fd); //udev.close_touchpad(fd);
-
-// var poll_fds = [_]os.pollfd{
-//     .{ .fd = fd, .events = os.POLLIN, .revents = undefined },
-// };
-
-// const events = try os.poll(&poll_fds, std.math.maxInt(i32));
-// if (events == 0) continue;
-
-// https://gist.github.com/marler8997/7f2e1b6a3ce938285c620c642c3e581a
-
-// ray.InitWindow(
-//     SCREEN_WIDTH,
-//     SCREEN_HEIGHT,
-//     "Cleartouch - Touchpad Visualizer",
-// );
-// defer ray.CloseWindow();
-
-// ray.SetTargetFPS(60);
-
-// var touchPositions: [MAX_TOUCH_POINTS]ray.Vector2 =
-//     [1]ray.Vector2{std.mem.zeroes(ray.struct_Vector2)} ** MAX_TOUCH_POINTS;
-
-// while (!ray.WindowShouldClose()) {
-//     ray.BeginDrawing();
-//     defer ray.EndDrawing();
-
-//     {
-//         var i: u64 = 0;
-//         while (i < MAX_TOUCH_POINTS) : (i += 1) {
-//             touchPositions[i] = ray.GetTouchPosition(@intCast(c_int, i));
-//         }
-//     }
-
-//     ray.ClearBackground(ray.RAYWHITE);
-
-//     {
-//         var i: c_uint = 0;
-//         while (i < MAX_TOUCH_POINTS) : (i += 1) {
-//             if ((touchPositions[i].x > 0) and (touchPositions[i].y > 0)) {
-//                 ray.DrawCircleV(touchPositions[i], 34, ORANGE);
-//                 ray.DrawText(
-//                     ray.TextFormat("%d", i),
-//                     @floatToInt(c_int, touchPositions[i].x - 10),
-//                     @floatToInt(c_int, touchPositions[i].y - 70),
-//                     40,
-//                     ray.BLACK,
-//                 );
-//             }
-//         }
-//     }
-// }
-// }
+const SCREEN_WIDTH = 1200;
+const SCREEN_HEIGHT = 800;
 
 const log = std.log;
 
-// var stdout: ?anytype = null;
+var machine = mt.MTStateMachine{};
 
 pub fn main() !void {
-
     try pike.init();
     defer pike.deinit();
 
@@ -83,31 +25,89 @@ pub fn main() !void {
     defer notifier.deinit();
 
     const fd: os.fd_t = try udev.open_touchpad();
-    defer os.close(fd); //udev.close_touchpad(fd);
+    defer os.close(fd);
 
-    const handle: pike.Handle =
-        .{ .inner = fd, .wake_fn = wake };
-    try notifier.register(
-        &handle,
-        .{ .read = true, .write = false },
+    // Initialize visual
+    ray.InitWindow(
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        "Cleartouch - Touchpad Visualizer",
     );
+    defer ray.CloseWindow();
+    ray.SetTargetFPS(60);
+
+    const handle: pike.Handle = .{ .inner = fd, .wake_fn = wake };
+    try notifier.register(&handle, .{ .read = true, .write = false });
 
     var iter: u64 = 0;
     while (true) : (iter += 1) {
-        try notifier.poll(1000);
+        try notifier.poll(10);
         std.debug.print("loop {}\n", .{iter});
+
+        if (ray.WindowShouldClose()) break;
+
+        {
+            ray.BeginDrawing();
+            defer ray.EndDrawing();
+
+            ray.ClearBackground(ray.RAYWHITE);
+
+            for (machine.touches) |touch, i| {
+                const pos: ray.Vector2 = ray.Vector2{
+                    .x = @intToFloat(f32, touch.position_x),
+                    .y = @intToFloat(f32, touch.position_y),
+                };
+                ray.DrawCircleV(pos, 34, ORANGE);
+                ray.DrawText(
+                    ray.TextFormat("%d", i),
+                    @floatToInt(c_int, pos.x - 10),
+                    @floatToInt(c_int, pos.y - 70),
+                    40,
+                    ray.BLACK,
+                );
+            }
+        }
     }
+
+    // while (!ray.WindowShouldClose()) {
+    //     ray.BeginDrawing();
+    //     defer ray.EndDrawing();
+
+    //     {
+    //         var i: u64 = 0;
+    //         while (i < MAX_TOUCH_POINTS) : (i += 1) {
+    //             touchPositions[i] = ray.GetTouchPosition(@intCast(c_int, i));
+    //         }
+    //     }
+
+    //     ray.ClearBackground(ray.RAYWHITE);
+
+    //     {
+    //         var i: c_uint = 0;
+    //         while (i < MAX_TOUCH_POINTS) : (i += 1) {
+    //             if ((touchPositions[i].x > 0) and (touchPositions[i].y > 0)) {
+    //                 ray.DrawCircleV(touchPositions[i], 34, ORANGE);
+    //                 ray.DrawText(
+    //                     ray.TextFormat("%d", i),
+    //                     @floatToInt(c_int, touchPositions[i].x - 10),
+    //                     @floatToInt(c_int, touchPositions[i].y - 70),
+    //                     40,
+    //                     ray.BLACK,
+    //                 );
+    //             }
+    //         }
+    //     }
 }
 
 fn wake(handle: *pike.Handle, batch: *pike.Batch, opts: pike.WakeOptions) void {
     const stdout = std.io.getStdOut().writer();
     // var events: [100]mt.InputEvent = std.mem.zeroes([100]mt.InputEvent);
-    var events: [10]mt.InputEvent = undefined;
+    var events: [100]mt.InputEvent = undefined;
     if (opts.read_ready) {
         // const bytes = os.read(handle.inner, @ptrCast([*]u8, &events));
         const bytes = os.read(handle.inner, std.mem.sliceAsBytes(events[0..])) catch 0;
         if (bytes == 0) {
-            std.debug.print("read 0 bytes\n");
+            std.debug.print("read 0 bytes\n", .{});
             return;
         }
 
@@ -117,6 +117,9 @@ fn wake(handle: *pike.Handle, batch: *pike.Batch, opts: pike.WakeOptions) void {
 
         for (events[0..eventCount]) |event| {
             event.format(stdout) catch std.debug.print("uhoh\n", .{});
+            machine.process(&event) catch |err| {
+                std.debug.print("can't process: {}\n", .{err});
+            };
         }
         // std.debug.print("{s}", .{events});
     }
--- src/multitouch.zig
+++ src/multitouch.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const MAX_TOUCH_POINTS = 10;
+
 const linux = @cImport({
     @cInclude("linux/input.h");
     @cInclude("linux/input-event-codes.h");
@@ -52,6 +54,155 @@ pub const InputEvent = extern struct {
         const time: f64 =
             std.math.lossyCast(f64, input.time.tv_sec - _start_time) +
             std.math.lossyCast(f64, input.time.tv_usec) / 1e6;
-        try std.fmt.format(writer, "{{EV({d}, {d}, {d}) ({d:.3})}}", .{ input.type, input.value, input.code, time });
+
+        switch (input.type) {
+            linux.EV_KEY => try std.fmt.format(
+                writer,
+                "  EV_KEY(0x{X}, {d}) ({d:.3})\n",
+                .{ input.code, input.value, time },
+            ),
+            linux.EV_ABS => try std.fmt.format(
+                writer,
+                "  EV_ABS(0x{X}, {d}) ({d:.3})\n",
+                .{ input.code, input.value, time },
+            ),
+            linux.EV_MSC => try std.fmt.format(
+                writer,
+                "  EV_MSC(0x{X}, {d}) ({d:.3})\n",
+                .{ input.code, input.value, time },
+            ),
+            linux.EV_SYN => try std.fmt.format(
+                writer,
+                "  EV_SYN() ({d:.3})\n",
+                .{time},
+            ),
+            else => try std.fmt.format(
+                writer,
+                "  EV(0x{X}, 0x{X}, {d}) ({d:.3})\n",
+                .{ input.type, input.code, input.value, time },
+            ),
+        }
+    }
+};
+
+const TouchData = struct {
+    used: bool = false,
+
+    tracking_id: i32 = -1,
+
+    position_x: i32,
+    position_y: i32,
+
+    pressure: i32,
+    distance: i32,
+
+    touch_major: i32,
+    touch_minor: i32,
+    width_major: i32,
+    width_minor: i32,
+    orientation: i32,
+
+    tool_x: i32,
+    tool_y: i32,
+    tool_type: i32,
+
+    pub fn reset(self: *TouchData) void {
+        self.used = false;
+
+        self.tracking_id = -1;
+
+        self.position_x = 0;
+        self.position_y = 0;
+
+        self.pressure = 0;
+        self.distance = 0;
+
+        self.touch_major = 0;
+        self.touch_minor = 0;
+        self.width_major = 0;
+        self.width_minor = 0;
+        self.orientation = 0;
+
+        self.tool_x = 0;
+        self.tool_y = 0;
+        self.tool_type = 0;
+    }
+};
+
+const MTError = error{ SlotOutOfBounds, SlotIsNull, BadState };
+
+const MTState = enum {
+    loading,
+    read_ready,
+    needs_reset,
+};
+
+pub const MTStateMachine = struct {
+    state: MTState = MTState.loading,
+
+    slot: ?usize = null,
+
+    touches: [MAX_TOUCH_POINTS]TouchData =
+        [1]TouchData{std.mem.zeroes(TouchData)} ** MAX_TOUCH_POINTS,
+
+    pub fn reset(self: *MTStateMachine) void {
+        self.state = MTState.loading;
+
+        self.slot = null;
+
+        for (self.touches) |_, i| {
+            self.touches[i].used = false;
+            // touch.used = false;
+        }
+    }
+
+    // fn set_slot(comptime field, slot: i32, value: i32) !void {
+    // }
+
+    pub fn process(self: *MTStateMachine, input: *const InputEvent) !void {
+        switch (input.type) {
+            linux.EV_KEY => {},
+            linux.EV_ABS => {
+                switch (self.state) {
+                    MTState.loading => {},
+                    MTState.needs_reset => self.reset(),
+                    MTState.read_ready => {}, // return MTError.BadState,
+                }
+
+                const slot = self.slot orelse 0;
+                const touch = &self.touches[slot];
+                switch (input.code) {
+                    linux.ABS_MT_SLOT => {
+                        if (input.value >= 0 and input.value < MAX_TOUCH_POINTS) {
+                            self.slot = @intCast(usize, input.value);
+                            self.touches[self.slot.?].used = true;
+                        } else {
+                            return MTError.SlotOutOfBounds;
+                        }
+                    },
+                    linux.ABS_MT_TRACKING_ID => {
+                        if (input.value < 0) {
+                            touch.used = false;
+                        } else {
+                            touch.tracking_id = input.value;
+                        }
+                    },
+                    linux.ABS_MT_TOUCH_MAJOR => touch.touch_major = input.value,
+                    linux.ABS_MT_TOUCH_MINOR => touch.touch_minor = input.value,
+                    linux.ABS_MT_POSITION_X => touch.position_x = input.value,
+                    linux.ABS_MT_POSITION_Y => touch.position_y = input.value,
+                    else => {},
+                }
+            },
+            linux.EV_MSC => {},
+            linux.EV_SYN => {
+                self.state = MTState.read_ready;
+            },
+            else => {},
+        }
+    }
+
+    pub fn is_read_ready(self: *MTStateMachine) bool {
+        return self.state == MTState.read_ready;
     }
 };
